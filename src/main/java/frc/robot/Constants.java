ce6c12f551ac3049d74f9762c3e9528029cf4d39
ReidS28 <strawj3434@gmail.com>
2025-01-20 18:18:13 -0500

    Added blenkin

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public final class BlinkinConstants {
+    public final static int kBlinkinPort = 0;
+  } 
 }
---
+++ src/main/java/frc/robot/subsystems/BlinkinSubsystem.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.BlinkinConstants;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+
+public class BlinkinSubsystem extends SubsystemBase {
+
+  private final ShuffleboardTab m_tab = Shuffleboard.getTab("Blinkin");
+
+  private final Spark blinkin;
+
+  public BlinkinSubsystem() {
+    blinkin = new Spark(BlinkinConstants.kBlinkinPort);
+    m_tab.add(blinkin.toString(), blinkin);
+    setDefault();
+  }
+
+  /**
+   * Sets the LED pattern for the REV Blinkin LED Driver.
+   *
+   * The pattern code determines the color or animation displayed by the LEDs.
+   * A full list of pattern codes and their meanings can be found in the REV
+   * Blinkin LED Driver User Manual:
+   * https://www.revrobotics.com/content/docs/REV-11-1105-UM.pdf#page=14
+   *
+   * @param pattern A double value representing the desired LED pattern code.
+   *
+   */
+  public void setBlinkin(double pattern) {
+    blinkin.set(pattern);
+  }
+
+  public void setDefault(){
+    setBlinkin(.69);
+  }
+
+}
