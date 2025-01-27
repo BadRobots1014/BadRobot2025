8cdd9b209fb9dd96f6455071d787549449dd60ad
Michael Kelbick <kelbick.3@dublinstudents.net>
2025-01-27 10:21:34 -0500

    Remove unneeded comments from TurnToThetaCommand.java
    Removed comments from TurnToThetaCommand.java that were left over from the example command file

--- src/main/java/frc/robot/commands/TurnToThetaCommand.java
+++ src/main/java/frc/robot/commands/TurnToThetaCommand.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
@@ -12,7 +8,6 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** An example command that uses an example subsystem. */
 public class TurnToThetaCommand extends SwerveDriveCommand {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
 
@@ -50,20 +45,13 @@ public class TurnToThetaCommand extends SwerveDriveCommand {
 
     turningPID = new PIDController(1.5, 0, 0);
   }
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     currentTheta = m_subsystem.getYaw();
 
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     // double currentAngle = m_subsystem.getRotation2d().getDegrees();
@@ -81,11 +69,9 @@ public class TurnToThetaCommand extends SwerveDriveCommand {
     super.execute();
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
