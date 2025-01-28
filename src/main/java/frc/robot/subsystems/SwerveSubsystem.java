bcd60008116128b0cfe8e1454305750316e13db8
Bad Robots Driver Station <kubik.2@dublinstudents.net>
2025-01-27 19:33:06 -0500

    Small fix
    might work

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -139,11 +139,10 @@ public class SwerveSubsystem extends SubsystemBase {
     p,i,d
   );
 
-  // The gyro
-  //private final AHRS gyro = new AHRS(SPI.Port.kMXP); this is old
-   private final AHRS gru = new AHRS(NavXComType.kMXP_SPI);
+  // The gru
+  private final AHRS gru = new AHRS(NavXComType.kMXP_SPI);
 
-  // Gyro data shenanigans
+  // Gru data shenanigans
   public void resetPose() {
     gru.reset();
     gru.resetDisplacement();
@@ -168,7 +167,7 @@ public class SwerveSubsystem extends SubsystemBase {
   public double getY() {return gru.getDisplacementY() + offsetY;}
   public double getXSpeed() {return gru.getVelocityX();}
   public double getYSpeed() {return gru.getVelocityY();}
-  public double getTurnSpeed() {return gru.getRate();}
+  public double getTurnSpeed() {return gru.getRate()/180*Math.PI;}
   public Pose2d getPose() {return new Pose2d(getX(), getY(), getRotation2d());}
 
   public ChassisSpeeds getRobotRelativeSpeeds() {return new ChassisSpeeds(getXSpeed(), getYSpeed(), getTurnSpeed());}
