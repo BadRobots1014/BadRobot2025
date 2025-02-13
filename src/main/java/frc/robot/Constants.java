185b23666084e72148b4ea7a63c175e367e07593
Bad Robots Driver Station <kubik.2@dublinstudents.net>
2025-02-13 15:01:53 -0500

    ratio

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,7 +31,7 @@ public final class Constants {
   public static final class ModuleConstants {
 
     public static final double kWheelDiameterMeters = 0.102;
-    public static final double kDriveMotorGearRatio = 1/8.14;
+    public static final double kDriveMotorGearRatio = 1/9.1d;
     public static final double kTurningMotorGearRatio = 1/12.8;
     public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
     public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
